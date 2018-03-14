b55da5253699eee875abc9a7f35f8dc77f030bc1
pbahrs <pbahrs@us.ibm.com>
2018-03-14 15:28:19 -0500

    Create security.tf

---
+++ security.tf
@@ -0,0 +1,29 @@
+# create a security group to limit communications with the virtual servers
+# below are the policies for the group
+
+resource "ibm_security_group" "sg1" {
+    name = "${var.userids}-securitygroup"
+    description = "allow my app traffic"
+}
+
+#create a rule to limit traffic to https on public network
+resource "ibm_security_group_rule" "allow_port_443" {
+    direction = "ingress"
+    ether_type = "IPv4"
+    port_range_min = 443
+    port_range_max = 443
+    protocol = "tcp"
+    remote_ip = 0.0.0.0/8
+    security_group_id = ${ibm_security_group.sg1.id}
+}
+
+#create a rule to limit traffic to ssh on private network
+resource "ibm_security_group_rule" "allow_port_22" {
+    direction = "ingress"
+    ether_type = "IPv4"
+    port_range_min = 22
+    port_range_max = 22
+    protocol = "tcp"
+    remote_ip = 0.0.0.0/8
+    security_group_id = ${ibm_security_group.sg1.id}
+}
