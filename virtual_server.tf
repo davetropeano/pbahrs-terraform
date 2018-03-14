9d8bae6d5cdb48c399ef5bd16c212029947af07b
pbahrs <pbahrs@us.ibm.com>
2018-03-14 14:07:22 -0500

    Update virtual_server.tf

--- virtual_server.tf
+++ virtual_server.tf
@@ -3,7 +3,7 @@
 resource "ibm_compute_vm_instance" "burst_vm_instances" {
   count = "${var.vm_count}"
   os_reference_code = "${var.osrefcode}"
-  hostname = "${format("THINKburst-%02d", count.index + 1)}"
+  hostname = "${format("THINK_public_private-%02d", count.index + 1)}"
   domain = "${var.domain}"
   datacenter = "${var.datacenter}"
   file_storage_ids = ["${ibm_storage_file.Think_burst.id}"]
@@ -22,7 +22,7 @@ resource "ibm_compute_vm_instance" "burst_vm_instances" {
 resource "ibm_compute_vm_instance" "burst_vm_instances" {
   count = "${var.vm_count}"
   os_reference_code = "${var.osrefcode}"
-  hostname = "${format("THINKburst-%02d", count.index + 1)}"
+  hostname = "${format("THINK_privateonly-%02d", count.index + 1)}"
   domain = "${var.domain}"
   datacenter = "${var.datacenter}"
   file_storage_ids = ["${ibm_storage_file.Think_burst.id}"]
