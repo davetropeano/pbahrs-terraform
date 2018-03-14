bb647e99681ae7ebbaac8b7ef8df9d9dce18fce3
pbahrs <pbahrs@us.ibm.com>
2018-03-14 15:49:09 -0500

    Update security.tf

--- security.tf
+++ security.tf
@@ -13,7 +13,7 @@ resource "ibm_security_group_rule" "${var.userids}-allow_port_443" {
     port_range_min = 443
     port_range_max = 443
     protocol = "tcp"
-    remote_ip = 0.0.0.0/8
+    remote_ip = "0.0.0.0/8"
     security_group_id = ${ibm_security_group.publicsg.id}
 }
 
@@ -31,6 +31,6 @@ resource "ibm_security_group_rule" "${var.userids}-allow_port_22" {
     port_range_min = 22
     port_range_max = 22
     protocol = "tcp"
-    remote_ip = 0.0.0.0/8
+    remote_ip = "0.0.0.0/8"
     security_group_id = ${ibm_security_group.privatesg.id}
 }
