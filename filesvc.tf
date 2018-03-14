f050fa8851f630f0c296c0a4b50eaff37b97c5dd
pbahrs <pbahrs@us.ibm.com>
2018-03-14 14:02:53 -0500

    Update filesvc.tf

--- filesvc.tf
+++ filesvc.tf
@@ -1,7 +1,7 @@
 #Create file storage
-#resource "ibm_storage_file" "Think_burst" {
-#  type = "Performance"
-#  datacenter = "${var.datacenter}"
-#  capacity = "20"
-#  iops = "100"
-#}
+resource "ibm_storage_file" "Think_burst" {
+  type = "Performance"
+  datacenter = "${var.datacenter}"
+  capacity = "20"
+  iops = "100"
+}
