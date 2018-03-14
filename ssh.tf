95de35b39f97cb06fcda35831306e695e234dc98
pbahrs <pbahrs@us.ibm.com>
2018-03-14 15:19:55 -0500

    Update ssh.tf

--- ssh.tf
+++ ssh.tf
@@ -1,6 +1,6 @@
 #Create ssh keys for virtual guests
 resource "ibm_compute_ssh_key" "ssh_key" {
-  label = "${var.user}+"."+${var.ssh_label}"
+  label = "${var.userids}.${var.ssh_label}"
   notes = "${var.ssh_notes}"
   public_key = "${var.ssh_key}"
 }
