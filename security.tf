b2019079e2727a196494d1685471f34c70ffa4c4
pbahrs <pbahrs@us.ibm.com>
2018-03-15 02:18:07 -0400

    Update security.tf

--- security.tf
+++ security.tf
@@ -2,8 +2,8 @@
 # below are the policies for the group
 
 resource "ibm_security_group" "sg1" {
-    name = "${var.userids}-securitygroup"
-    description = "allow my app traffic"
+    name = "sg1"
+    description = "public app traffic"
 }
 
 #create a rule to limit traffic to https on public network
@@ -15,14 +15,15 @@ resource "ibm_security_group_rule" "port_443" {
     protocol = "tcp"
     remote_ip = "0.0.0.0/8"
     security_group_id = "${ibm_security_group.sg1.id}"
+    depends_on = ["ibm_security_group.sg1"]
 }
 
 # create a security group to limit communications on private network
 # below are the policies for the group
 
 resource "ibm_security_group" "sg2" {
-    name = "${var.userids}-securitygroup"
-    description = "allow my app traffic"
+    name = "sg2"
+    description = "private app traffic"
 }
 #create a rule to limit traffic to ssh on private network
 resource "ibm_security_group_rule" "port_22" {
