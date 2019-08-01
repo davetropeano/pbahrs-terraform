051b858d39fa904a8f12183f169c69967a43be1f
pbahrs <pbahrs@us.ibm.com>
2019-08-01 15:29:48 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
-#Define the providers to use
 provider "ibm" {
-  softlayer_username = "${var.classicIaaS_username}"
-  softlayer_api_key = "${var.classicIaaS_apikey}"
+  softlayer_username = "${var.softlayer_username}"
+  softlayer_api_key  = "${var.softlayer_api_key}"
+  ibmcloud_api_key    = "${var.ibmcloud_api_key}"
 }
