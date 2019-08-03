7b87cc7aab2c2e94a01f5c05c3fc44ab2096e61a
davetropeano <davetropeano@us.ibm.com>
2019-08-02 20:33:29 -0400

    added back IaaS username and API key

--- provider.tf
+++ provider.tf
@@ -1,2 +1,12 @@
+var "softlayer_username" {
+    description="IaaS username"
+}
+
+var "softlayer_api_key" {
+    description="IaaS API Key"
+}
+
 provider "ibm" {
+    softlayer_username = "${var.softlayer_username}"	
+    softlayer_api_key  = "${var.softlayer_api_key}"
 }
